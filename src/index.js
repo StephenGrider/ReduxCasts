37b938da862939de3ad71016600ee817631304c0
stephen grider <ste.grider@gmail.com>
2015-12-20 15:01:37 -0800

    imported react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // Create a new component.  This component should produce
 // some HTML
 const App = function() {
@@ -6,3 +8,4 @@ const App = function() {
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
+React.render(App);
