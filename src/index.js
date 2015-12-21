ef7fe815aac47ebcf78efafb12e4f9b7c018a709
stephen grider <ste.grider@gmail.com>
2015-12-20 17:32:11 -0800

    refactor app to a class

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Create a new component.  This component should produce
-// some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
