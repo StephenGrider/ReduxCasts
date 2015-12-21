8ca6acbb5fb358590a282ae1abc9a69edd9d5cfa
stephen grider <ste.grider@gmail.com>
2015-12-20 17:00:00 -0800

    state is updated whenever the input is changed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,11 @@ class SearchBar extends Component {
   }
 
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
