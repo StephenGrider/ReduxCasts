841482af9eed0887ef32ae6661618dbe0d55ac0c
stephen grider <ste.grider@gmail.com>
2015-12-20 16:52:50 -0800

    Added constructor

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
     return <input onChange={event => console.log(event.target.value)} />;
   }
