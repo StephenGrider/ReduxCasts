879db70eb623f68f2c076aee72d06e407b6319a8
stephen grider <ste.grider@gmail.com>
2016-01-01 12:10:29 -0800

    added render check to book detail

--- book_list/src/containers/book-detail.js
+++ book_list/src/containers/book-detail.js
@@ -3,8 +3,16 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
     );
   }
 }
--- book_list/src/reducers/reducer_books.js
+++ book_list/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'The Dark Tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 1 }
   ];
 }
