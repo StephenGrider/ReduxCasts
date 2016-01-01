63d3b8a6388d13739508fd9586fd11536a1a679f
stephen grider <ste.grider@gmail.com>
2016-01-01 11:53:57 -0800

    created bookdetail container

---
+++ book_list/src/containers/book-detail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    return (
+      <div>Book Detail!</div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
