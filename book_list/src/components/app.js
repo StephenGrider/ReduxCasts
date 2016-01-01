460f77a337c68ce099c07f72cfcd503c85226cb3
stephen grider <ste.grider@gmail.com>
2015-12-31 16:43:44 -0800

    finished promoting booklist to a container

--- book_list/src/components/app.js
+++ book_list/src/components/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
 
+import BookList from '../containers/book-list';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
