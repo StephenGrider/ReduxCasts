2c72908f968e09eff1a02ea7776ed1e1c9a1b93a
stephen grider <ste.grider@gmail.com>
2016-01-02 18:07:34 -0800

    added google maps lib

---
+++ weather/src/components/google_map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
+
+export default (props) => {
+  return (
+    <GoogleMapLoader
+      containerElement={ <div style={{height: '100%'}} /> }
+      googleMapElement={
+        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}}
+      }
+      />
+  );
+}
