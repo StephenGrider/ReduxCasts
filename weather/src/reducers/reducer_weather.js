76f9b9683bb29500c99bb29870bf1d90cc72df97
stephen grider <ste.grider@gmail.com>
2016-01-02 16:40:35 -0800

    added handler for fetchwather

--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -1,3 +1,9 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+    return [ action.payload.data, ...state ];
+  }
   return state;
 }
