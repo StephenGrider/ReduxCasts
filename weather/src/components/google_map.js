1f1df6135dd6db56261bbb28aa1e3f0125663eb0
stephen grider <ste.grider@gmail.com>
2016-01-02 18:14:25 -0800

    google maps component in use

--- weather/src/components/google_map.js
+++ weather/src/components/google_map.js
@@ -6,7 +6,7 @@ export default (props) => {
     <GoogleMapLoader
       containerElement={ <div style={{height: '100%'}} /> }
       googleMapElement={
-        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}}
+        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
       }
       />
   );
