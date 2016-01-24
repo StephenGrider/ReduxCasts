36a173ddc3ec31b590061c6105cde60145c4df8d
stephen grider <ste.grider@gmail.com>
2016-01-24 13:12:39 -0800

    added react router

---
+++ blog/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} />
+  </Provider>
+  , document.querySelector('.container'));
