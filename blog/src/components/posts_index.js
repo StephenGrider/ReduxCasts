0b8f17cf1f34ec52f41ecc96e315842a54c60aee
stephen grider <ste.grider@gmail.com>
2016-01-24 14:14:07 -0800

    added action creator

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
-    console.log('this would be a good time to call an action creator to fetch posts');
+    this.props.fetchPosts();
   }
 
   render() {
@@ -12,4 +14,4 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
