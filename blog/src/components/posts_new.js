37e2b6d0a27d1a81810a7dcf432d867e4f3ce96d
stephen grider <ste.grider@gmail.com>
2016-01-24 14:24:38 -0800

    added postsnew component

---
+++ blog/src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>Create Form</div>
+    );
+  }
+}
+
+export default PostsNew;
--- blog/src/routes.js
+++ blog/src/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={PostsNew} />
   </Route>
 );
