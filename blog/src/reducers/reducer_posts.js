38677855db8c9d3d72a2ed3c95c951984991d484
stephen grider <ste.grider@gmail.com>
2016-01-24 13:54:26 -0800

    added initial state

---
+++ blog/src/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+import { FETCH_POSTS } from '../actions/index';
+
+const INITIAL_STATE = { all: [], post: null };
+
+export default function(state = INITIAL_STATE, action) {
+  switch(action.type) {
+  case FETCH_POSTS:
+    
+  default:
+    return state;
+  }
+}
