37e96746816a3894e8bee7164498ab4c9c5fa2fc
stephen grider <ste.grider@gmail.com>
2016-01-24 13:58:05 -0800

    added case

--- blog/src/reducers/reducer_posts.js
+++ blog/src/reducers/reducer_posts.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { all: [], post: null };
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FETCH_POSTS:
-    
+    return { ...state, all: action.payload.data };
   default:
     return state;
   }
