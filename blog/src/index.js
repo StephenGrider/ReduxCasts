4623107ee9b6f01f435660196f1289df482152e8
stephen grider <ste.grider@gmail.com>
2016-01-24 13:46:15 -0800

    added index action creator

--- blog/src/index.js
+++ blog/src/index.js
@@ -5,8 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
