81ad5fb4cd9760cf37644645848f1a5ba1de60ca
stephen grider <ste.grider@gmail.com>
2016-01-24 13:39:40 -0800

    added indexroute

--- blog/src/routes.js
+++ blog/src/routes.js
@@ -2,15 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-
-const Greeting = () => {
-  return <div>Hey there!</div>;
-};
+import PostsIndex from './components/posts_index';
 
 export default (
   <Route path="/" component={App}>
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
+    <IndexRoute component={PostsIndex} />
   </Route>
 );
