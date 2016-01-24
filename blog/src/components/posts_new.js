559a9fee00c8d773ed386c9bbf3365c0f05139fd
stephen grider <ste.grider@gmail.com>
2016-01-24 15:06:29 -0800

    pass props to fields

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -3,22 +3,24 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
