1282e4bde98dc0a255f15f342dce11f2496cb662
stephen grider <ste.grider@gmail.com>
2016-01-24 13:27:26 -0800

    added route nesting

--- blog/src/components/app.js
+++ blog/src/components/app.js
@@ -4,7 +4,9 @@ import { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
--- blog/src/routes.js
+++ blog/src/routes.js
@@ -3,6 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+  return <div>Hey there!</div>;
+};
+
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
 );
