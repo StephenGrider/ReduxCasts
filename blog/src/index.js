ed26b4d70c45c2cfc389613a0cab6c0e9301c164
stephen grider <ste.grider@gmail.com>
2016-01-24 13:18:43 -0800

    added root route

--- blog/src/index.js
+++ blog/src/index.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
---
+++ blog/src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+
+export default (
+  <Route path="/" component={App} />
+);
