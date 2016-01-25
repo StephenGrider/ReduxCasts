698627d5cece8fd28378235a2ae66da50421232f
stephen grider <ste.grider@gmail.com>
2016-01-24 16:46:46 -0800

    navigate on delete

--- blog/src/components/posts_show.js
+++ blog/src/components/posts_show.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
 
   onDeleteClick() {
-    this.props.deletePost(this.props.params.id);
+    this.props.deletePost(this.props.params.id)
+      .then(() => { this.context.router.push('/'); });
   }
 
   render() {
