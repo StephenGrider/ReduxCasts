3cf4a1be79e672b3f7e57ce909dc463ad844c770
stephengrider <ste.grider@gmail.com>
2017-04-13 12:10:20 -0700

    added route definition

---
+++ blog/src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Posts Index
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
--- blog/src/index.js
+++ blog/src/index.js
@@ -4,26 +4,16 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() { return <div>Hello!</div> }
-}
-
-class Goodbye extends React.Component {
-  render() { return <div>Goodbye!</div> }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Header
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
