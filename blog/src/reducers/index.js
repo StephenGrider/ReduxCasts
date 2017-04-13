79998a1ba922d07a68206c259ab79e0a0b9211b5
stephengrider <ste.grider@gmail.com>
2017-04-13 12:58:25 -0700

    added posts reducer

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer
 });
 
 export default rootReducer;
