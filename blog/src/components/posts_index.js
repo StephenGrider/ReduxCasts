6e20e17895e1ad01d1fc73822f6fa0f5e351e4b0
stephengrider <ste.grider@gmail.com>
2017-04-13 13:43:03 -0700

    added shortcut

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +16,4 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
