7cb35d022cafe181441d0cd170e63542bbd26f0d
stephengrider <ste.grider@gmail.com>
2017-04-20 11:02:30 -0700

    added field

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     );
