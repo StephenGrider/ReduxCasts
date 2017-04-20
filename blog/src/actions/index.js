0d84533a00102870b5cd2cea3c5d5321908995dc
stephengrider <ste.grider@gmail.com>
2017-04-20 15:15:50 -0700

    navigate on post create

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -15,8 +15,9 @@ export function fetchPosts() {
   };
 }
 
-export function createPost(values) {
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
 
   return {
     type: CREATE_POST,
--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -25,7 +25,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
