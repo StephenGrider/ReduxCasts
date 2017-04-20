fae44575c118bc28001c914e9ec0c2b2d9781871
stephengrider <ste.grider@gmail.com>
2017-04-20 15:49:47 -0700

    ownprops

--- blog/src/components/posts_show.js
+++ blog/src/components/posts_show.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
     return (
       <div>
         Posts Show!
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
