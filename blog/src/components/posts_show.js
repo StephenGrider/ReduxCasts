d7b6b247b2776b630ac4e7bb68649e8803c5dc8c
stephengrider <ste.grider@gmail.com>
2017-04-20 15:20:18 -0700

    added posts show

---
+++ blog/src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  };
+}
+
+export default PostsShow;
--- blog/src/index.js
+++ blog/src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
