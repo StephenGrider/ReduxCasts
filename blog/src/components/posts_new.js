0bf04dad56b59cc48b18b6ca1907182323ba7ac6
stephengrider <ste.grider@gmail.com>
2017-04-20 14:00:43 -0700

    conditional errors

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -11,7 +11,7 @@ class PostsNew extends Component {
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        {field.meta.touched ? field.meta.error : ''}
       </div>
     );
   }
