844eec736fa4827bc5bcb5d433503724ed017a52
stephengrider <ste.grider@gmail.com>
2017-04-20 16:26:58 -0700

    wrapup

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP1234';
