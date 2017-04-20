1e2b20c071ba3925776529bd56e8a0828733aa16
stephengrider <ste.grider@gmail.com>
2017-04-20 14:15:43 -0700

    conditional styling

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -3,15 +3,20 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
