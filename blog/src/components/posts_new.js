1a71ee198337c27c8a5f815d6c654e477bf4d8cb
stephengrider <ste.grider@gmail.com>
2017-04-20 10:44:28 -0700

    field setup

--- blog/src/components/posts_new.js
+++ blog/src/components/posts_new.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
