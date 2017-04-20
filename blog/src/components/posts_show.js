873308520bf13dd89185c9b60815751c34b0b339
stephengrider <ste.grider@gmail.com>
2017-04-20 15:57:25 -0700

    check for post

--- blog/src/components/posts_show.js
+++ blog/src/components/posts_show.js
@@ -9,9 +9,17 @@ class PostsShow extends Component {
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
