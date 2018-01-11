1137337d951ca2ed7fff8831ed1868eb1b7b7f41
Simone Icardi <info@simoneicardi.com>
2018-01-11 20:19:26 +0100

    commented out WeatherList container that has issue to go ahead step by step

--- weather/src/components/app.js
+++ weather/src/components/app.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Component } from "react";
 
 import SearchBar from "../containers/search_bar";
-import WeatherList from "../containers/weather_list";
+//import WeatherList from "../containers/weather_list";
+//AGGIUNGERE SOTTO <WeatherList />
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
-        <WeatherList />
+        
       </div>
     );
   }
