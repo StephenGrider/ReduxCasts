c257b7c77499081796a499e921aa77ed8c9a8797
Simone Icardi <info@simoneicardi.com>
2018-01-15 19:57:50 +0100

    fixed sparkline module bug and macth the course step

--- weather/src/containers/weather_list.js
+++ weather/src/containers/weather_list.js
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Chart from "../components/chart";
-import GoogleMap from "../components/google_map";
+//import Chart from "../components/chart";
+import {
+  Sparklines,
+  SparklinesLine
+} from "react-sparklines";
+//import GoogleMap from "../components/google_map";
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    const pressures = cityData.list.map(weather => weather.main.pressure);
+    /*const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
-    const { lon, lat } = cityData.city.coord;
+    const { lon, lat } = cityData.city.coord;*/
+      
+      
+      //ADD
+      /*<td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="K" /></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>*/
 
     return (
       <tr key={name}>
-        <td><GoogleMap lon={lon} lat={lat} /></td>
-        <td><Chart data={temps} color="orange" units="K" /></td>
-        <td><Chart data={pressures} color="green" units="hPa" /></td>
-        <td><Chart data={humidities} color="black" units="%" /></td>
+        <td>{name}</td>
+        <td>
+            <Sparklines data={temps}>
+              <SparklinesLine color="red" />
+            </Sparklines>
+        </td>
       </tr>
     );
   }
