263025b1b74b50f739fd4cbd8cf8c5cef46e5cd9
shubham15098 <shubham15098@iiitd.ac.in>
2018-10-03 02:10:44 +0530

    Update video_detail.js
    changed the string concatenation format from js6

--- video_browser/src/components/video_detail.js
+++ video_browser/src/components/video_detail.js
@@ -6,7 +6,7 @@ const VideoDetail = ({ video }) => {
   }
 
   const videoId = video.id.videoId;
-  const url = `https://www.youtube.com/embed/${videoId}`;
+  const url = 'https://www.youtube.com/embed/' + 'videoId';
 
   return (
     <div className="video-detail col-md-8">
