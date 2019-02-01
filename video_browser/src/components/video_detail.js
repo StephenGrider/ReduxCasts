6599369140ff4aa7b2c5370b599dae9cb53863a8
Joshua Aguilar <joshuaaguilar20@gmail.com>
2019-01-31 16:07:23 -0800

    Update video_detail.js
    Allows for fullscreen.

--- video_browser/src/components/video_detail.js
+++ video_browser/src/components/video_detail.js
@@ -11,7 +11,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={url} />
+        <iframe className="embed-responsive-item" src={url} allowfullscreen="0" />
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
